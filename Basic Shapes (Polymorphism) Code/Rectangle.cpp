07cfeaf4a744985846af1a4355bc539ea10b6c17
jm underwood <jm.underwood@hotmail.com>
2026-05-04 21:50:47 -0400

    Wrote up Rectangle.cpp

--- Basic Shapes (Polymorphism) Code/Rectangle.cpp
+++ Basic Shapes (Polymorphism) Code/Rectangle.cpp
@@ -1,1 +1,22 @@
 #include "Rectangle.h"
+
+Rectangle::Rectangle(double l, double w, string n) : length(l), width(w)
+{
+	setName(n);
+	calcArea();
+}
+
+void Rectangle::calcArea()
+{
+	setArea(length * width);
+}
+
+void Rectangle::setLength(double l)
+{
+	length = l;
+}
+
+void Rectangle::setWidth(double w)
+{
+	width = w;
+}
