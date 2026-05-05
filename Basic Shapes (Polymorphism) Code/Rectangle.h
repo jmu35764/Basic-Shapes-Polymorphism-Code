2fa733d310fb4fc7f9ef084a967c4fb38ee955e7
jm underwood <jm.underwood@hotmail.com>
2026-05-04 21:44:24 -0400

    Completed Rectangle.h

--- Basic Shapes (Polymorphism) Code/Rectangle.h
+++ Basic Shapes (Polymorphism) Code/Rectangle.h
@@ -1,5 +1,40 @@
-#pragma once
-class Rectangle
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include "BasicShape.h"
+
+class Rectangle : public BasicShape
 {
+private:
+	double length;
+	double width;
+
+public:
+	
+	Rectangle(double l, double w, string n = "Rectangle");
+
+
+	void calcArea() override;
+	
+	//Setters
+	void setLength(double l);
+
+	void setWidth(double w);
+
+	//Getters
+	double getLength() const
+	{
+		return length;
+	}
+
+	double getWidth() const
+	{
+		return width;
+	}
+
+
+
 };
+
+#endif
 
