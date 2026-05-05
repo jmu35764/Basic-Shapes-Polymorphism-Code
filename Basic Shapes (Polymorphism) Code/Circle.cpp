00b3fcae7e8400e8f429f583db92022b4da6b057
jm underwood <jm.underwood@hotmail.com>
2026-05-04 21:37:47 -0400

    Finished Circle.cpp code

--- Basic Shapes (Polymorphism) Code/Circle.cpp
+++ Basic Shapes (Polymorphism) Code/Circle.cpp
@@ -16,3 +16,18 @@ void Circle::calcArea()
 	const double pi = numbers::pi;
 	setArea(radius * radius * pi);
 }
+
+void Circle::setxCenter(double x)
+{
+	xCenter = x;
+}
+
+void Circle::setyCenter(double y)
+{
+	yCenter = y;
+}
+
+void Circle::setRadius(double r)
+{
+	radius = r;
+}
