2d16fd036907cc19c8471ad8dbbc39b257e18353
jm underwood <jm.underwood@hotmail.com>
2026-05-04 20:55:31 -0400

    Created circle.h

--- Basic Shapes (Polymorphism) Code/Circle.h
+++ Basic Shapes (Polymorphism) Code/Circle.h
@@ -1,5 +1,41 @@
-#pragma once
-class Circle
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include "BasicShape.h"
+#include <string>
+
+class Circle : public BasicShape
 {
+private:
+	double xCenter;
+	double yCenter;
+	double radius;
+
+public:
+	Circle(double x, double y, double r, string n = "Cirle");
+
+	//Setters
+	void setxCenter(double x);
+
+	void setyCenter(double y);
+
+	void setRadius(double r);
+
+	//Getters
+	double getxCenter() const
+	{
+		return xCenter;
+	}
+
+	double getyCenter() const
+	{
+		return yCenter;
+	}
+
+	double getRadius() const
+	{
+		return radius;
+	}
 };
 
+#endif
