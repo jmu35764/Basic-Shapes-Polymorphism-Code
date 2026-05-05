f9a92c53a56666b2611610ea87be9ff4419ec395
jm underwood <jm.underwood@hotmail.com>
2026-05-04 21:59:36 -0400

    Wrote up Sqaure.h

--- Basic Shapes (Polymorphism) Code/Square.h
+++ Basic Shapes (Polymorphism) Code/Square.h
@@ -1,5 +1,28 @@
-#pragma once
-class Square
+#ifndef SQUARE_H
+#define SQAURE_H
+
+#include "Rectangle.h"
+#include <string>
+
+using namespace std;
+
+class Square : public Rectangle
 {
+private:
+	double side;
+
+public:
+	// Constructor
+	Square(double s, string n = "Square");
+
+	//Setters
+	void setSide(double s);
+
+	double getSide() const
+	{
+		return side;
+	}
+
 };
 
+#endif
